4984b348260bee11913fce1a13d8bab366364e16
andrew <andrew@revvo.ai>
2020-12-01 20:51:53 -0800

    day 4 part 2

--- 4.py
+++ 4.py
@@ -1,12 +1,18 @@
 lower = 178416
 bound = 676461
 
-def is_valid(i):
-  as_str = str(i)
+def is_valid(value):
+  as_str = str(value)
   has_adjacent_digits = False
   for i in range(0, 5):
-    if as_str[i] == as_str[i + 1]:
+    if as_str[i] != as_str[i + 1]:
+      continue
+
+    has_next_same = i + 2 < 6 and as_str[i + 1] == as_str[i + 2]
+    has_prev_same = i != 0 and as_str[i - 1] == as_str[i]
+    if not has_next_same and not has_prev_same:
       has_adjacent_digits = True
+      break
 
   if not has_adjacent_digits:
     return False
